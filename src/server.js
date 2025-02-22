a8ef071a71ede6b30920da6c25d41d2511034c0c
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-02-22 17:48:55 -0300

    Cors e Readme.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 
@@ -6,6 +7,7 @@ const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const port = 3000
 
